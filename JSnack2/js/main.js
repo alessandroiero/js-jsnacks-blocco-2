41ac8ef96a947b05a6bbe0fc1dca33dc69c5d855
alessandroiero <alessandro.iero@outlook.it>
2023-02-27 11:55:29 +0100

    Esercizio 1

---
+++ JSnack2/js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+// Inserisci un numero, se è pari stampa il numero,
+// se è dispari stampa il numero successivo
+
+let numero = Number(prompt('Inserisci un numero'));
+
+if (numero % 2 == 0) {
+    console.log(numero);
+}
+else{
+    console.log(numero + 1);
+}
