b2b7ab325103c3db1d4e1d3d2736db328a07fa52
alessandroiero <alessandro.iero@outlook.it>
2023-02-27 12:29:12 +0100

    Add Falsa Lista

--- JSSnack3/js/main.js
+++ JSSnack3/js/main.js
@@ -1,16 +1,20 @@
 // Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
 'use strict';
 
-let nomi= ['Giovanni','Francesco','Roberto','Alessandro','Filippo'];
-let cognomi=['cognome1','cognome2','cognome3','cognome4','cognome5'];
+const nomi= ['Giovanni','Francesco','Roberto','Alessandro','Filippo'];
+const cognomi=['cognome1','cognome2','cognome3','cognome4','cognome5'];
+const fullNameList=[];
 
 for(let i=0; i<3;i++){
     const randomNomi = nomi[Math.floor(Math.random() * nomi.length)];
     const randomCognomi = cognomi[Math.floor(Math.random() * cognomi.length)];
-    console.log(randomNomi,randomCognomi);
+    const newFullName= `${randomNomi} ${randomCognomi}`
+
+
+    fullNameList.push(newFullName);
 };
 
-
+console.log(fullNameList);
 
 
 
