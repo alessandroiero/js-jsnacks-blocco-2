b65d3ea92213551b04f3f66dab573e9f6317bda2
alessandroiero <alessandro.iero@outlook.it>
2023-03-01 11:07:22 +0100

    Aggiunta Somma

--- JSnack1/js/main.js
+++ JSnack1/js/main.js
@@ -3,15 +3,23 @@
 // Esegui questo programma in due versioni, con il for e con il while.
 
 // Ciclo For
+let sum=0;
 for(let i = 0 ; i < 5 ;i++){
     const number= Number(prompt('numero'));
+    sum += number;
     console.log(number);
 }
 
+console.log(sum);
+
 // Ciclo While
+let sumTwo=0;
 let count=0;
 while(count<5){
     const numberTwo= Number(prompt('numero'));
     console.log(numberTwo);
+    sumTwo += numberTwo;
     count++;
 }
+
+console.log(sumTwo);
